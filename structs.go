bcad2a63bb50b8f5282e046fda831b2f585d1f51
Andres Morales <meliand11@yahoo.com>
2025-11-17 15:08:56 -0500

    using a struct to get data

--- structs.go
+++ structs.go
@@ -2,21 +2,43 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
+//defining your own custom types
+//I f you put an uppercase on types it allows the function to be used in many fuctions and files
+
+type user struct {
+	firstName string
+	lastName  string
+	birthDate string
+	createdAt time.Time
+}
+
 func main() {
-	firstName := getUserData("Please enter your first name: ")
-	lastName := getUserData("Please enter your last name: ")
-	birthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
+	userfirstName := getUserData("Please enter your first name: ")
+	userlastName := getUserData("Please enter your last name: ")
+	userbirthDate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
+
+	var appUser user
+
+	//you can omit the firstname: same for other values but I would keep it as it could get confusing
+	//you can omit a whole value
+	appUser = user{
+		firstName: userfirstName,
+		lastName:  userlastName,
+		birthDate: userbirthDate,
+		createdAt: time.Now(),
+	}
 
 	// ... do something awesome with that gathered data!
 
-	outputUserDetails(firstName, lastName, birthdate)
+	outputUserDetails(appUser)
 }
 
-func outputUserDetails(firstName, lastName, birthdate string) {
+func outputUserDetails(u user) {
 	//..
-	fmt.Println(firstName, lastName, birthdate)
+	fmt.Println(u.firstName, u.lastName, u.birthDate)
 }
 
 func getUserData(promptText string) string {
