fb1c53ce63ff933104fbe83e3703cd7279170b74
Andres Morales <meliand11@yahoo.com>
2025-11-18 15:48:48 -0500

    struct added to a new package

--- structs.go
+++ structs.go
@@ -1,20 +1,13 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"time"
+
+	"example.com/structs/user"
 )
 
 //defining your own custom types
 //I f you put an uppercase on types it allows the function to be used in many fuctions and files
-
-type user struct {
-	firstName string
-	lastName  string
-	birthDate string
-	createdAt time.Time
-}
 
 func main() {
 	userfirstName := getUserData("Please enter your first name: ")
@@ -23,59 +16,18 @@ func main() {
 
 	//you can omit the firstname: same for other values but I would keep it as it could get confusing
 	//you can omit a whole value
+	var appUser *user.User
 
-	var appUser, err = newUser(userfirstName, userlastName, userbirthDate)
-
+	appUser, err := user.NewUser(userfirstName, userlastName, userbirthDate)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	// ... do something awesome with that gathered data!
 
-	appUser.outputUserDetails()
-	appUser.clearUserName()
-	appUser.outputUserDetails()
-}
-
-/*
-	in order to practice with pointers instead of copying data I will use a pointer so that
-
-The same stored pointer data is used. Doing it this way saves data. This isn't needed
-most of the time but helps with practice
-*/
-
-// this is how you add a stuct to a function to create methods
-// you don't have to put a derefence when calling the struct but if you don't a copy of the data will be created.
-func (u *user) outputUserDetails() {
-	//..
-
-	/*fmt.Println((*u).firstName, (*u).lastName, (*u).birthDate)
-	technically you need to dereference the pointer as yI have done above
-	but golang offers the option to not do that with structs so the below works.
-	*/
-	fmt.Println(u.firstName, u.lastName, u.birthDate)
-}
-
-// this shows how to edit struct values
-// when editing a struct make sure to actually get the pointer by addin * infront of the pointer to dereference.
-func (u *user) clearUserName() {
-	u.firstName = ""
-	u.lastName = ""
-}
-
-// this is the constructor it makes the struct and can allow easy validation
-func newUser(firstName, lastName, birthDate string) (*user, error) {
-	//setting validation for struct
-	if firstName == "" || lastName == "" || birthDate == "" {
-		return nil, errors.New("firstname, lastname and birthdate are required")
-	}
-
-	return &user{
-		firstName: firstName,
-		lastName:  lastName,
-		birthDate: birthDate,
-		createdAt: time.Now(),
-	}, nil
+	appUser.OutputUserDetails()
+	appUser.ClearUserName()
+	appUser.OutputUserDetails()
 }
 
 func getUserData(promptText string) string {
---
+++ user/user.go
@@ -0,0 +1,56 @@
+package user
+
+import (
+	"errors"
+	"fmt"
+	"time"
+)
+
+// To expose variables in a struct the variable must be upper case
+type User struct {
+	firstName string
+	lastName  string
+	birthDate string
+	createdAt time.Time
+}
+
+/*
+	in order to practice with pointers instead of copying data I will use a pointer so that
+
+The same stored pointer data is used. Doing it this way saves data. This isn't needed
+most of the time but helps with practice
+*/
+
+// this is how you add a stuct to a function to create methods
+// you don't have to put a derefence when calling the struct but if you don't a copy of the data will be created.
+func (u *User) OutputUserDetails() {
+	//..
+
+	/*fmt.Println((*u).firstName, (*u).lastName, (*u).birthDate)
+	technically you need to dereference the pointer as yI have done above
+	but golang offers the option to not do that with structs so the below works.
+	*/
+	fmt.Println(u.firstName, u.lastName, u.birthDate)
+}
+
+// this shows how to edit struct values
+// when editing a struct make sure to actually get the pointer by addin * infront of the pointer to dereference.
+func (u *User) ClearUserName() {
+	u.firstName = ""
+	u.lastName = ""
+}
+
+// this is the constructor it makes the struct and can allow easy validation
+func NewUser(firstName, lastName, birthDate string) (*User, error) {
+	//setting validation for struct
+	if firstName == "" || lastName == "" || birthDate == "" {
+		return nil, errors.New("firstname, lastname and birthdate are required")
+	}
+
+	return &User{
+		firstName: firstName,
+		lastName:  lastName,
+		birthDate: birthDate,
+		createdAt: time.Now(),
+	}, nil
+}
